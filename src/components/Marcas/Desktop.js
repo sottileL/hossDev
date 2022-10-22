8efce730e66d09be842da6d44eeb613a0340ad39
Luciano <lucianodsottile@gmail.com>
2022-10-21 23:27:48 -0300

    imported table, need to make data creation wrk

--- src/components/Marcas/Desktop.js
+++ src/components/Marcas/Desktop.js
@@ -7,10 +7,24 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import {styled} from '@mui/styles';
-import {Divider} from '@mui/material';
+import {Divider, Table} from '@mui/material';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import rectangle from '../../images/rectangle.png';
 import darklogo from '../../images/darklogo.png';
 import logo from '../../images/logo.png';
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9)
+];
 
 const TitleTypography = styled(Typography)(() => ({
     fontSize: '1.2rem!important',
@@ -135,6 +149,35 @@ const MarcasDesktop = () => (
                 </FooterTypography>
             </Grid>
         </Grid>
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth: 650}} size="small" aria-label="a dense table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align="right">Calories</TableCell>
+                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map(row => (
+                        <TableRow
+                            key={row.name}
+                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.carbs}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     </Grid>
 );
 
