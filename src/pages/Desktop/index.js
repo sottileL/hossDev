b3161560787d8c625f293b69a84dca773715c9bf
Luciano <lucianodsottile@gmail.com>
2022-10-23 19:55:57 -0300

    attempt on react router

--- src/pages/Desktop/index.js
+++ src/pages/Desktop/index.js
@@ -1,8 +1,16 @@
 import React, {useRef} from 'react';
 import Grid from '@mui/material/Grid';
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
 import Header from '../../components/Header/Desktop';
 import Footer from '../../components/Footer/FooterDesktop';
+import Nosotros from '../../components/Nosotros/NosotrosDesktop';
+import Comisiones from '../../components/Comisiones/ComisionesDesktop';
+import Resumen from '../../components/Resumen/ResumenDesktop';
 import Contacto from '../../components/Contacto/ContactoDesktop';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -14,7 +22,6 @@ const Desktop = () => {
     const resumenRef = useRef(null);
     const contactoRef = useRef(null);
 
-    // react router?
     const handleClickMenu = section => {
         if (section === 'nosotros') {
             nosotrosRef.current.scrollIntoView({behavior: 'smooth'});
@@ -24,7 +31,7 @@ const Desktop = () => {
             comisionesRef.current.scrollIntoView({behavior: 'smooth'});
         }
 
-        if (section === 'c') {
+        if (section === 'resumen') {
             resumenRef.current.scrollIntoView({behavior: 'smooth'});
         }
 
@@ -34,15 +41,36 @@ const Desktop = () => {
     };
 
     return (
-        <Grid direction="column" width="100%" id="inicio" ref={nosotrosRef}>
-            <Header handleClickMenu={handleClickMenu}/>
-            <Grid item id="contacto" ref={contactoRef}>
-                <Contacto/>
+        <Router>
+            <Grid direction="column" width="100%" id="inicio" ref={nosotrosRef}>
+                <Header handleClickMenu={handleClickMenu}/>
+                <Switch>
+                    <Route exact path="/">
+                        <Grid item id="nosotros">
+                            <Nosotros/>
+                        </Grid>
+                    </Route>
+                    <Route path="/comisiones">
+                        <Grid item id="comisiones">
+                            <Comisiones/>
+                        </Grid>
+                    </Route>
+                    <Route path="/resumen">
+                        <Grid item id="resumen">
+                            <Resumen/>
+                        </Grid>
+                    </Route>
+                    <Route path="/contacto">
+                        <Grid item id="contacto">
+                            <Contacto/>
+                        </Grid>
+                    </Route>
+                    <Grid item id="footer">
+                        <Footer/>
+                    </Grid>
+                </Switch>
             </Grid>
-            <Grid item id="footer">
-                <Footer/>
-            </Grid>
-        </Grid>
+        </Router>
     );
 };
 
